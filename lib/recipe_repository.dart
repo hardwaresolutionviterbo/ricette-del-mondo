89a369209c399e89f8457c46344063a901886995
hardwaresolutionviterbo <hardwaresolutionviterbo@gmail.com>
2026-09-14 00:36:48 +0200

    Add files via upload

---
+++ lib/recipe_repository.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'recipe.dart';
+class RecipeRepository {
+  Future<List<Recipe>> loadRecipes() async {
+    final raw = await rootBundle.loadString('assets/recipes.json');
+    final data = jsonDecode(raw) as List<dynamic>;
+    return data.map((x) => Recipe.fromJson(x as Map<String,dynamic>)).toList();
+  }
+}
